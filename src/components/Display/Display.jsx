1e343a5b443fd08fe66f6c24f482257fee7a42f4
-MJ <mollyl.johnson@att.net>
2019-01-05 14:06:43 -0600

    finished part 2

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -4,10 +4,14 @@ import './Display.css'
 
 const Display = ({ displayValue }) => (
     <div className="display-container">
-        <p>{displayValue}</p>
+        <p className="display-value">
+            {displayValue}
+        </p>
     </div>
 );
 
 Display.propTypes = { displayValue: PropTypes.string.isRequired };
+
+Display.defaultProps = { displayValue: 'default' }
 
 export default Display;
--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,14 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Key from '../Key/Key';
+import './Keypad.css';
 
-const Keypad = ({ callOperator, numbers, operators, setOperator, updateDisplay }) => <div className="keypad-container" />;
+const Keypad = ({ callOperator, handleKeyPress,  numbers, operators, setOperator, updateDisplay }) => {
+    
+    const numberKeys = numbers.map((num, i) => (
+        <Key
+            handleKeyPress={handleKeyPress}
+            key={`${num}${i}`}
+            keyAction={updateDisplay}
+            keyType="number-key"
+            keyValue={num}
+        />)
+    );
+    const operatorKeys = operators.map((op, i) => (
+        <Key
+            handleKeyPress={handleKeyPress}
+            key={`${op}${i}`}
+            keyAction={setOperator}
+            keyType="operator-key"
+            keyValue={op}
+        />)
+    );
+    
+    return (
+        <div className="keypad-container">
+            <div className="numbers-container">
+                {numberKeys}
+            </div>
+            <div className="operators-container">
+                {operatorKeys}
+            </div>
+            <div className="submit-container">
+                <Key
+                    handleKeyPress={handleKeyPress}
+                    keyAction={callOperator}
+                    keyType="submit-key"
+                    keyValue="="
+                />
+            </div>
+        </div>
+    );
+}
 
 Keypad.propTypes = {
     callOperator: PropTypes.func.isRequired,
+    handleKeyPress: PropTypes.func.isRequired,
     numbers: PropTypes.array.isRequired,
     operators: PropTypes.array.isRequired,
     setOperator: PropTypes.func.isRequired,
     updateDisplay: PropTypes.func.isRequired,
+}
+
+Keypad.defaultProps = {
+    numbers: [],
+    operators: [],
 }
 
 export default Keypad;
